0614f1698076a173f96815e428d701b77c7921cb
Theo Lemaire <theo.lemaire@epfl.ch>
2021-06-08 16:09:48 +0200

    added responses figure

--- utils.py
+++ utils.py
@@ -3,11 +3,12 @@
 # @Email: theo.lemaire@epfl.ch
 # @Date:   2021-06-08 14:56:14
 # @Last Modified by:   Theo Lemaire
-# @Last Modified time: 2021-06-08 15:28:32
+# @Last Modified time: 2021-06-08 16:04:16
 
 import os
+import pickle
 from argparse import ArgumentParser
-
+from PySONIC.utils import logger
 from config import dataroot
 
 
@@ -33,3 +34,10 @@ def saveFigs(figs):
         os.mkdir(figroot)
     for k, fig in figs.items():
         fig.savefig(os.path.join(figroot, f'{k}.pdf'), transparent=True)
+
+
+def loadData(fpath):
+    logger.info('Loading data from "%s"', os.path.basename(fpath))
+    with open(fpath, 'rb') as fh:
+        frame = pickle.load(fh)
+    return frame['data'], frame['meta']
