28d8b1566f3ef04ba804673ad6b522a70a787642
Theo Lemaire <theo.lemaire@epfl.ch>
2021-06-25 18:17:28 +0200

    fixed mpi issues in test_bundle

--- test_bundle.py
+++ test_bundle.py
@@ -3,7 +3,7 @@
 # @Email: theo.lemaire@epfl.ch
 # @Date:   2021-06-21 13:50:43
 # @Last Modified by:   Theo Lemaire
-# @Last Modified time: 2021-06-24 19:40:20
+# @Last Modified time: 2021-06-25 18:16:39
 
 import logging
 import numpy as np
@@ -56,42 +56,37 @@ w = 2e-3  # FWHM (m)
 sigma = GaussianAcousticSource.from_FWHM(w)  # m
 
 # Bundle parameters
-n = 8
-bundle_contours = circleContour(100e-6, n=n)
+bundle_contours = circleContour(100e-6, n=100)
 length = 10e-3  # m
-# f_kwargs = dict(fiberD_hists=fiberD_ref_hist, pratio=0.3, a=a, fs=fs)
-f_kwargs = dict(fiberD_hists=fiberD_ref_hist, pratio=0.01, a=a, fs=fs)
+f_kwargs = dict(fiberD_hists=fiberD_ref_hist, pratio=0.05, a=a, fs=fs)
 
-# # Pulsing parameters
-# min_npulses = 10
-# PDs = {'UN': 10e-3, 'MY': 0.1e-3}  # s
-# PRFs = {'UN': 50., 'MY': 200.}  # Hz
-# npulses = dict(zip(PRFs.keys(), getNPulses(min_npulses, PRFs.values())))
+# Pulsing parameters
+min_npulses = 10
+PDs = {'UN': 10e-3, 'MY': 0.1e-3}  # s
+PRFs = {'UN': 50., 'MY': 200.}  # Hz
+npulses = dict(zip(PRFs.keys(), getNPulses(min_npulses, PRFs.values())))
 
-# # Get fiber-specific single-pulse threshold excitation amplitudes
-# # for their respective pulse durations
-# fibers = {k: getFiber(k, a=a, fs=fs, fiberL=length) for k in ['UN', 'MY']}
-# drive = AcousticDrive(Fdrive)
-# Athrs = {k: NeuronalBilayerSonophore(a, fiber.pneuron).titrate(
-#     drive, PulsedProtocol(PDs[k], 10e-3), fs=fs) for k, fiber in fibers.items()}
-# s = 'fiber-specific thresholds: '
-# s += ', '.join([f'A_{k} = {v * 1e-3:.1f} kPa'for k, v in Athrs.items()])
-# s += f' (ratio = {max(Athrs.values()) / min(Athrs.values()):.2f})'
-# logger.info(s)
+# Get fiber-specific single-pulse threshold excitation amplitudes
+# for their respective pulse durations
+fibers = {k: getFiber(k, a=a, fs=fs, fiberL=length) for k in ['UN', 'MY']}
+drive = AcousticDrive(Fdrive)
+Athrs = {k: NeuronalBilayerSonophore(a, fiber.pneuron).titrate(
+    drive, PulsedProtocol(PDs[k], 10e-3), fs=fs) for k, fiber in fibers.items()}
+s = 'fiber-specific thresholds: '
+s += ', '.join([f'A_{k} = {v * 1e-3:.1f} kPa'for k, v in Athrs.items()])
+s += f' (ratio = {max(Athrs.values()) / min(Athrs.values()):.2f})'
+logger.info(s)
 
-# # Determine acoustic source amplitude and pulsed protocols modulation factors
-# # from single-pulse thresholds
-# Amin = min(Athrs.values())
-# Adrive = 1.1 * Amin
-# factors = {k: v / Amin for k, v in Athrs.items()}
-# flist = list(factors.values())
-# source = GaussianAcousticSource(0., sigma, Fdrive, Adrive)
-# pp = ProtocolArray(
-#     [f * getPulseTrainProtocol(PDs[k], npulses[k], PRFs[k]) for k, f in factors.items()],
-#     minimize_overlap=True)
-
-source = GaussianAcousticSource(0., sigma, Fdrive, 100e3)
-pp = PulsedProtocol(1e-3, 10e-3)
+# Determine acoustic source amplitude and pulsed protocols modulation factors
+# from single-pulse thresholds
+Amin = min(Athrs.values())
+Adrive = 1.1 * Amin
+factors = {k: v / Amin for k, v in Athrs.items()}
+flist = list(factors.values())
+source = GaussianAcousticSource(0., sigma, Fdrive, Adrive)
+pp = ProtocolArray(
+    [f * getPulseTrainProtocol(PDs[k], npulses[k], PRFs[k]) for k, f in factors.items()],
+    minimize_overlap=True)
 
 
 if __name__ == '__main__':
@@ -105,15 +100,15 @@ if __name__ == '__main__':
     figs['cross-section'] = bundle.plotCrossSection()
     figs['offsets'] = bundle.plotLongitudinalOffsets()
 
-    # def simfunc(fiber, pos):
-    #     ''' Simulation function to apply to all bundle fibers. '''
-    #     source.x0 = - pos[0]
-    #     return fiber.simAndSave(
-    #         source, pp, outputdir=bundle_root, overwrite=False, full_output=False)
+    def simfunc(fiber, pos):
+        ''' Simulation function to apply to all bundle fibers. '''
+        source.x0 = - pos[0]
+        return fiber.simAndSave(
+            source, pp, outputdir=bundle_root, overwrite=False, full_output=False)
 
-    # # Apply simulation to all fibers
-    # fpaths = bundle.forall(simfunc, mpi=args.mpi)
-    # figs['raster'] = bundle.rasterPlot(fpaths)
+    # Apply simulation to all fibers
+    fpaths = bundle.forall(simfunc, mpi=args.mpi)
+    figs['raster'] = bundle.rasterPlot(fpaths)
 
     # f_offsets = {
     #     'f1': [150e-6, 0.],
